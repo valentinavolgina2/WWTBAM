e873e756b2d2004b24306e034622c8d30522c726
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-26 20:17:43 -0700

    Added show user profile integration test

---
+++ spec/features/show_user_profile_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature 'show user profile', type: :feature do
+  let(:user) { create :user, name: 'Test user' }
+
+  let!(:games) { [
+    create(:game, user: user, created_at: '2022.01.01 11:00', prize: 1000, current_level: 3, fifty_fifty_used: true),
+    create(:game, user: user, created_at: '2022.03.04 15:00', prize: 0, current_level: 0, is_failed: true, finished_at: '2021.03.04 15:05')] }
+
+  scenario 'success' do
+    visit '/'
+
+    click_link "user_#{user.id}"
+
+    expect(page).to have_current_path "/users/#{user.id}"
+
+    # check user name on the page
+    expect(page).to have_content 'Test user'
+
+    # check games' statuses
+    expect(page).to have_content 'fail'
+    expect(page).to have_content 'in progress'
+
+    # check games' dates
+    expect(page).to have_content '04 Mar 15:00'
+    expect(page).to have_content '01 Jan 11:00'
+
+    # check games' current levels
+    expect(page).to have_content '0'
+    expect(page).to have_content '3'
+
+    # check games' prizes
+    expect(page).to have_content '$0'
+    expect(page).to have_content '$1,000'
+
+    # check 50/50 help is used
+    expect(find('.game-help-used').text).to eq('50/50')
+
+    # no edit button
+    expect(page).not_to have_content 'Change name and password'
+  end
+end
